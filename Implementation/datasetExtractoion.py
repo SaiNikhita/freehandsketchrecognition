becc2e161df26221d9c57e9d7dc9436565b3702a
Sai Nikhita Nayani <sainikhitanayani@gmail.com>
2019-06-23 12:44:19 +0000

    Implementation of getting all samples using os library

--- Implementation/datasetExtractoion.py
+++ Implementation/datasetExtractoion.py
@@ -4,6 +4,7 @@ DATASET_PATH = r'C:\Users\Anudeep\Desktop\Projects\Free Hand Sketch Recognition\
 
 for roots, directories, files in os.walk(DATASET_PATH):
     for file in files:
-        print("Path ", roots)
-        print("File ", file)
+        print("Path of directory ", roots)
+        print("Filename ", file)
+        print("Path of sample ", os.path.join(roots, file))
         print("Label ", roots.split('\\')[-1])
