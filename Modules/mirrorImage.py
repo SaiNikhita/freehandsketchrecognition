100f677ba6ad8a33d4c4f0fff9c45af1aed7830e
Vanitha Kunta <vanithakunta2406@gmail.com>
2019-06-24 05:32:52 +0000

    Function for mirroring across vertical axis

---
+++ Modules/mirrorImage.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+
+def mirrorImage(img):
+    mirrored_img = cv2.flip(img, 1)
+    cv2.imwrite("mirror_image.png", mirrored_img)
+
+
+img = cv2.imread('482.png', 0)
+mirrorImage(img)
