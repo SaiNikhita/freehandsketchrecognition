ba427121c9e2a233a5122656b54370d824b19822
Sai Nikhita Nayani <sainikhitanayani@gmail.com>
2019-06-24 13:26:28 +0000

    Function to crop image

--- Modules/crop.py
+++ Modules/crop.py
@@ -2,5 +2,5 @@ def crop_image(image, height, width):
     img_height, img_width = image.shape[:2]
     top = (img_height - height) // 2
     left = (img_width - width) // 2
-    cropped = img[top:img_height-top, left:img_width-left]
+    cropped = image[top:img_height-top, left:img_width-left]
     return cropped
