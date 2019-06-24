642f28f7b9232b9588dec7c25c3bc3c4802e9314
Sai Nikhita Nayani <sainikhitanayani@gmail.com>
2019-06-24 06:12:17 +0000

    Function to rotate image by an angle

--- Modules/rotate.py
+++ Modules/rotate.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 
 
-def rotate(img_erosion, angle):
-    (h, w) = img_erosion.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY
-    rotated = cv2.warpAffine(img_erosion, M, (nW, nH), borderValue=(255, 255, 255))
+def rotate_image(img, angle):
+    (height, width) = img.shape[:2]
+    (centerX, centerY) = (width // 2, height // 2)
+    rotation_matrix = cv2.getRotationMatrix2D((centerX, centerY), angle, 1.0)
+    cos = np.abs(rotation_matrix[0, 0])
+    sin = np.abs(rotation_matrix[0, 1])
+    new_width = int((height * sin) + (width * cos))
+    new_height = int((height * cos) + (width * sin))
+    rotation_matrix[0, 2] += (new_width / 2) - centerX
+    rotation_matrix[1, 2] += (new_height / 2) - centerY
+    rotated = cv2.warpAffine(img, rotation_matrix, (new_width, new_height), borderValue=(255, 255, 255))
     return rotated
