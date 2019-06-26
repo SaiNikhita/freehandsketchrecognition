b4e9d4c30de46a10f1b1bc356df4bbb7fe17b657
Vanitha Kunta <vanithakunta2406@gmail.com>
2019-06-26 10:07:09 +0000

    Fine tuning of VGG16 model for fully connected layers with 2 classes

---
+++ Implementation/finetuning_dense_layers.py
@@ -0,0 +1,89 @@
+import os
+import sklearn
+import time
+import numpy as np
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import merge, Input
+from keras.models import Model
+
+img_data_list = []
+path = os.getcwd()
+for dir in os.listdir(path):
+ if dir in ['airplane', 'alarm clock']:
+   images = os.listdir(os.path.join(path, dir))
+   for imge in images:
+     img_path = path + '/'+ dir + '/'+ imge
+     #print(img_path)
+     img = image.load_img(img_path, target_size=(224, 224))
+     x = image.img_to_array(img)
+     x = np.expand_dims(x, axis=0)
+     x = preprocess_input(x)
+     #print('Input image shape:', x.shape)
+     img_data_list.append(x)
+     
+img_data = np.array(img_data_list)
+#print (img_data.shape)
+img_data=np.rollaxis(img_data,1,0)
+#print (img_data.shape)
+img_data=img_data[0]
+#print (img_data.shape)
+
+
+num_classes = 2
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:961]=0
+labels[961:]=1
+
+names = ['airplane','alarm clock']
+
+
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+
+
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+
+image_input = Input(shape=(224, 224, 3))
+
+model = VGG16(input_tensor=image_input, include_top=True,weights='imagenet')
+model.summary()
+
+last_layer = model.get_layer('block5_pool').output
+x= Flatten(name='flatten')(last_layer)
+x = Dense(128, activation='relu', name='fc1')(x)
+x = Dense(128, activation='relu', name='fc2')(x)
+out = Dense(num_classes, activation='softmax', name='output')(x)
+custom_vgg_model2 = Model(image_input, out)
+custom_vgg_model2.summary()
+
+# freeze all the layers except the dense layers
+
+for layer in custom_vgg_model2.layers[:-3]:
+    layer.trainable = False
+custom_vgg_model2.summary()
+
+custom_vgg_model2.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
+
+t=time.time()
+hist = custom_vgg_model2.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
+print('Training time: %s' % (t - time.time()))
+(loss, accuracy) = custom_vgg_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
+
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+
+
+
+
