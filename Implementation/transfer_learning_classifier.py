346115c8897d0fdd72163e8ea0a4fe77db206176
Vanitha Kunta <vanithakunta2406@gmail.com>
2019-06-27 06:45:24 +0000

    Transfer learning for classifier alone

---
+++ Implementation/transfer_learning_classifier.py
@@ -0,0 +1,117 @@
+import os
+import time
+import sklearn
+import numpy as np
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications.vgg16 import VGG16
+from imagenet_utils import decode_predictions
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import merge, Input
+from keras.models import Model
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+# Loading the training data
+
+PATH = os.getcwd()
+img_data_list=[]
+
+for dir in os.listdir(path):
+	if dir in ['apple', 'banana']:
+	    images = os.listdir(os.path.join(path, dir))
+    	for img in img_list:
+    		img_path = data_path + '/'+ dataset + '/'+ img
+    		img = image.load_img(img_path, target_size=(224, 224))
+    		x = image.img_to_array(img)
+    		x = np.expand_dims(x, axis=0)
+    		x = preprocess_input(x)
+    		print('Input image shape:', x.shape)
+    		img_data_list.append(x)
+
+img_data = np.array(img_data_list)
+print (img_data.shape)
+img_data=np.rollaxis(img_data,1,0)
+print (img_data.shape)
+img_data=img_data[0]
+print (img_data.shape)
+
+
+# Define number of classes
+
+num_classes = 2
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:961]=0
+labels[961:]=1
+names = ['apple','banana']
+
+# convert class labels to one-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+
+image_input = Input(shape=(224, 224, 3))
+
+model = VGG16(input_tensor = image_input, include_top = True, weights = 'imagenet')
+model.summary()
+
+last_layer = model.get_layer('fc2').output
+out = Dense(num_classes, activation='softmax', name='output')(last_layer)
+custom_vgg_model2 = Model(image_input, out)
+custom_vgg_model2.summary()
+
+# Train classifier and freeze other layers
+
+for layer in custom_vgg_model2.layers[:-1]:
+	layer.trainable = False
+custom_vgg_model2.summary()
+custom_vgg_model2.layers[3].trainable
+
+
+custom_vgg_model2.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
+
+t=time.time()
+hist = custom_vgg_model2.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
+print('Training time: %s' % (t - time.time()))
+(loss, accuracy) = custom_vgg_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
+
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+
+
+# plotting losses and accuracy
+
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(12)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+plt.style.use(['classic'])
